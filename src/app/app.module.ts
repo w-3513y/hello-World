c77a0bffaddc3fa86316987c37f2ff4625f39701
Wesley Ferreira <114656@pmenos.com.br>
2022-02-23 23:34:13 -0300

    criando componentes usando: ng g c nome-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuSegundoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/meu-primeiro/meu-primeiro.component.ts
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,4 +1,9 @@
 import { Component } from "@angular/core";
 
-@Component({})
-class MeuPrimeiroComponent {}
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro component com Angular 2</p>
+  `
+})
+export class MeuPrimeiroComponent {}
